f045ebf5da7e54f16e20e0a12176ddde027e8e58
zeynepalaka <zeynepalakaa@gmail.com>
2022-01-24 23:19:19 +0300

    webService

---
+++ app.js
@@ -0,0 +1,22 @@
+/*header burger menü click to 180 deg x */
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if( $(window).scrollTop()>30){
+            $('.header').css({'background':'#FFD700','box-shadow':'0 .2rem .5rem  rgba(0,0,0,.4)'});
+
+        }
+        else{
+            $('.header').css({'background':'none','box-shadow':'none'});
+        }
+    });
+
+});
